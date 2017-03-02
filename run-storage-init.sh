0107e787213fda56a7af4efa38dff0506c923c7e
mroeder <roeder@informatik.uni-leipzig.de>
2017-03-02 12:08:48 +0100

    updated the run storage init script.

--- run-storage-init.sh
+++ run-storage-init.sh
@@ -1,5 +1,2 @@
 # Runs the initialization script for the Storage of the HOBBIT Platform.
-docker exec -it vos bash
-cd /opt/virtuoso-opensource/var/lib/virtuoso/db
-./storage-init.sh
-exit
+docker exec -it vos bash ./storage-init.sh
