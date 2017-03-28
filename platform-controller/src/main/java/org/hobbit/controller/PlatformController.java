e2d90037889cb393ddcc16480f57b6d5bca4166f
mroeder <roeder@informatik.uni-leipzig.de>
2017-03-28 18:34:41 +0200

    increased version.

--- platform-controller/src/main/java/org/hobbit/controller/PlatformController.java
+++ platform-controller/src/main/java/org/hobbit/controller/PlatformController.java
@@ -92,7 +92,7 @@ public class PlatformController extends AbstractCommandReceivingComponent
      * 
      * TODO Find a way to load the version automatically from the pom file.
      */
-    public static final String PLATFORM_VERSION = "1.0.2";
+    public static final String PLATFORM_VERSION = "1.0.3";
 
     private static final String DEPLOY_ENV = System.getProperty("DEPLOY_ENV", "production");
     private static final String DEPLOY_ENV_TESTING = "testing";
