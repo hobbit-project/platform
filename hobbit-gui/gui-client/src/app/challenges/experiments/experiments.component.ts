3466e73e339450c7d68218b3f7e49dc6a80daf1e
Marc Zöller <m.zoller@usu.de>
2017-11-03 12:51:32 +0100

    Fix #137

--- hobbit-gui/gui-client/src/app/challenges/experiments/experiments.component.ts
+++ hobbit-gui/gui-client/src/app/challenges/experiments/experiments.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Challenge, ExperimentCount } from './../../model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BackendService } from './../../backend.service';
@@ -14,21 +15,35 @@ export class ExperimentsComponent implements OnInit {
   private counts: ExperimentCount[] = [];
   private selectedCount: ExperimentCount;
 
-  constructor(private bs: BackendService, private activatedRoute: ActivatedRoute, private router: Router) { }
+  constructor(private bs: BackendService, private activatedRoute: ActivatedRoute, private router: Router,
+    private location: Location) { }
 
   ngOnInit() {
     const id = this.activatedRoute.snapshot.params['id'];
-    const taskId = this.activatedRoute.snapshot.queryParams['task'];
     this.bs.getChallenge(id).subscribe(data => {
       this.challenge = data;
     });
 
     this.bs.countExperiments(id).subscribe(data => {
       this.counts = data;
+      let taskId = this.activatedRoute.snapshot.params['task'];
       if (taskId) {
+        taskId = decodeURIComponent(taskId);
         this.selectedCount = this.counts.find(value => value.challengeTask.id === taskId);
       }
     });
+  }
+
+  handleSelect(event) {
+    this.selectedCount = event.data;
+    const url = this.router.createUrlTree([], { relativeTo: this.activatedRoute }).toString();
+
+    const tokens = url.split('/');
+    if (tokens[tokens.length - 1] !== 'experiments')
+      tokens.splice(-1, 1);
+    tokens.push(encodeURIComponent(this.selectedCount.challengeTask.id));
+
+    this.location.go(tokens.join('/'));
   }
 
 }
--- hobbit-gui/gui-client/src/app/challenges/leaderboards/leaderboards.component.ts
+++ hobbit-gui/gui-client/src/app/challenges/leaderboards/leaderboards.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Challenge, ChallengeTask } from './../../model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BackendService } from './../../backend.service';
@@ -14,17 +15,31 @@ export class LeaderboardsComponent implements OnInit {
   private tasks: ChallengeTask[] = [];
   private selectedTask: ChallengeTask;
 
-  constructor(private bs: BackendService, private activatedRoute: ActivatedRoute, private router: Router) { }
+  constructor(private bs: BackendService, private activatedRoute: ActivatedRoute, private router: Router,
+    private location: Location) { }
 
   ngOnInit() {
     const id = this.activatedRoute.snapshot.params['id'];
-    const taskId = this.activatedRoute.snapshot.queryParams['task'];
     this.bs.getChallenge(id).subscribe(data => {
       this.challenge = data;
+      let taskId = this.activatedRoute.snapshot.params['task'];
       if (taskId) {
+        taskId = decodeURIComponent(taskId);
         this.selectedTask = this.challenge.tasks.find(task => task.id === taskId);
       }
     });
+  }
+
+  handleSelect(event) {
+    this.selectedTask = event.data;
+    const url = this.router.createUrlTree([], { relativeTo: this.activatedRoute }).toString();
+
+    const tokens = url.split('/');
+    if (tokens[tokens.length - 1] !== 'leaderboards')
+      tokens.splice(-1, 1);
+    tokens.push(encodeURIComponent(this.selectedTask.id));
+
+    this.location.go(tokens.join('/'));
   }
 
 }
