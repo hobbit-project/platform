4d9fac7f3f339c3f6798acc473f123becb2d83e2
Denis Kuchelev <denkv@users.noreply.github.com>
2017-11-06 15:25:11 +0100

    Increase the core version to 1.0.7 and platform version to 1.0.13

--- platform-controller/src/main/java/org/hobbit/controller/PlatformController.java
+++ platform-controller/src/main/java/org/hobbit/controller/PlatformController.java
@@ -96,7 +96,7 @@ public class PlatformController extends AbstractCommandReceivingComponent
      *
      * TODO Find a way to load the version automatically from the pom file.
      */
-    public static final String PLATFORM_VERSION = "1.0.12";
+    public static final String PLATFORM_VERSION = "1.0.13";
 
     private static final String DEPLOY_ENV = System.getProperty("DEPLOY_ENV", "production");
     private static final String DEPLOY_ENV_TESTING = "testing";
