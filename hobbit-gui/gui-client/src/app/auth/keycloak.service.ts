fdb5f10f65cadc4d7790b9f5b84f128b76cf675b
Marc Zöller <m.zoller@usu.de>
2017-11-06 09:46:15 +0100

    Improve error message

--- hobbit-gui/gui-client/src/app/auth/keycloak.service.ts
+++ hobbit-gui/gui-client/src/app/auth/keycloak.service.ts
@@ -32,7 +32,8 @@ export class KeycloakService {
       };
       req.onerror = (e) => {
         console.error(`Error on fetching keycloak configuration: status = ${req.status} ${req.statusText}`);
-        document.getElementById('rootError').innerHTML = 'Failed to init user authentication. Please contact a system administrator.';
+        document.getElementById('rootError').innerHTML = 'Failed to init user authentication. This is probably ' +
+         'caused by a unsuccessfull communication with the UI backend. Please contact a system administrator.';
 
         reject('Unable to query keycloak config.');
       };
