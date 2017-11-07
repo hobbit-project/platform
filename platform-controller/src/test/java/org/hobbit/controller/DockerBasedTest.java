ff64c94911d7c2b0e5d71c3ced58453eda0aaa79
Micha <michael.roeder@uni-paderborn.de>
2017-11-07 17:04:32 +0100

    Fixed problems in the docker based test base class that lead to NullPointerExceptions.

--- platform-controller/src/test/java/org/hobbit/controller/DockerBasedTest.java
+++ platform-controller/src/test/java/org/hobbit/controller/DockerBasedTest.java
@@ -30,13 +30,17 @@ import com.spotify.docker.client.messages.Image;
 public class DockerBasedTest {
     protected DockerClient dockerClient;
     protected static final String busyboxImageName = "busybox:latest";
-    protected static final String[] sleepCommand = {"sleep", "20s"};
+    protected static final String[] sleepCommand = { "sleep", "20s" };
 
     protected boolean findImageWithTag(final String id, final List<Image> images) {
-        for (Image image : images) {
-            for (String tag : image.repoTags()) {
-                if (tag.contains(id)) {
-                    return true;
+        if (images != null) {
+            for (Image image : images) {
+                if (image.repoTags() != null) {
+                    for (String tag : image.repoTags()) {
+                        if (tag.contains(id)) {
+                            return true;
+                        }
+                    }
                 }
             }
         }
@@ -46,7 +50,6 @@ public class DockerBasedTest {
     @Before
     public void initClient() throws Exception {
         dockerClient = DefaultDockerClient.fromEnv().build();
-
         // check if busybox is present
         List<Image> images = dockerClient.listImages(DockerClient.ListImagesParam.allImages());
         if (!findImageWithTag(busyboxImageName, images)) {
