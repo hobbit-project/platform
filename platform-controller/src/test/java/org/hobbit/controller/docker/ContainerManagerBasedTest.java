f0360cd780ea7e0621368805e6c6de075d156bd7
Denis Kuchelev <denkv@users.noreply.github.com>
2017-11-22 12:18:58 +0100

    Remove unused main method in test class

--- platform-controller/src/test/java/org/hobbit/controller/docker/ContainerManagerBasedTest.java
+++ platform-controller/src/test/java/org/hobbit/controller/docker/ContainerManagerBasedTest.java
@@ -57,41 +57,4 @@ public class ContainerManagerBasedTest extends DockerBasedTest {
             }
         }
     }
-
-    public static void main(String[] args) throws Exception {
-        DockerClient dockerClient = DefaultDockerClient.fromEnv().build();
-
-        String containerName = "riak-test4";
-        ContainerConfig.Builder cfgBuilder = ContainerConfig.builder();
-        cfgBuilder.image("basho/riak-kv");
-        cfgBuilder.hostname(containerName);
-
-        cfgBuilder.env(Arrays.asList("HOBBIT_CONTAINER_NAME=riak-kv-eb1cb01e6b1348bc9dff895d29f7fa29",
-                "CLUSTER_NAME=riakkv", "HOBBIT_RABBIT_HOST=rabbit", "HOBBIT_SESSION_ID=1481122628848",
-                "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin", "OS_FAMILY=ubuntu",
-                "OS_VERSION=14.04", "OS_FLAVOR=trusty", "DEBIAN_FRONTEND=noninteractive",
-                "DEBCONF_NONINTERACTIVE_SEEN=true", "RIAK_VERSION=2.1.4", "RIAK_FLAVOR=KV", "RIAK_HOME=/usr/lib/riak"));
-
-        // trigger creation
-        ContainerConfig cfg = cfgBuilder.build();
-        try {
-            ContainerCreation resp = dockerClient.createContainer(cfg, containerName);
-            String containerId = resp.id();
-            // disconnect the container from every network it might be connected
-            // to
-            ContainerInfo info = dockerClient.inspectContainer(containerId);
-            Map<String, AttachedNetwork> networks = info.networkSettings().networks();
-            for (String networkName : networks.keySet()) {
-                dockerClient.disconnectFromNetwork(containerId, networkName);
-            }
-            // connect to hobbit network
-            dockerClient.connectToNetwork(resp.id(), "hobbit");
-            // return new container id
-            dockerClient.startContainer(containerId);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        dockerClient.close();
-    }
 }
