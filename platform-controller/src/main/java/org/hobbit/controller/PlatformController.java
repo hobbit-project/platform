1857bf34f9dbb3fc3769f7a589f620d5c07e09be
Micha <michael.roeder@uni-paderborn.de>
2017-12-21 13:02:46 +0100

    Increased version to 1.0.14. Increased the version of hobbit core to 1.0.8.

--- platform-controller/src/main/java/org/hobbit/controller/PlatformController.java
+++ platform-controller/src/main/java/org/hobbit/controller/PlatformController.java
@@ -96,7 +96,7 @@ public class PlatformController extends AbstractCommandReceivingComponent
      *
      * TODO Find a way to load the version automatically from the pom file.
      */
-    public static final String PLATFORM_VERSION = "1.0.13";
+    public static final String PLATFORM_VERSION = "1.0.14";
 
     private static final String DEPLOY_ENV = System.getProperty("DEPLOY_ENV", "production");
     private static final String DEPLOY_ENV_TESTING = "testing";
@@ -745,7 +745,6 @@ public class PlatformController extends AbstractCommandReceivingComponent
      */
     private void closeChallenge(String challengeUri) {
         LOGGER.info("Closing challenge {}...", challengeUri);
-        Calendar now = Calendar.getInstance(Constants.DEFAULT_TIME_ZONE);
         // send SPARQL query to close the challenge
         String query = SparqlQueries.getCloseChallengeQuery(challengeUri, Constants.CHALLENGE_DEFINITION_GRAPH_URI);
         if (query == null) {
@@ -759,7 +758,6 @@ public class PlatformController extends AbstractCommandReceivingComponent
                     challengeUri);
             return;
         }
-
         executeChallengeExperiments(challengeUri);
     }
 
