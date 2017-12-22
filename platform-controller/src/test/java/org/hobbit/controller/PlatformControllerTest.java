47ff2530314df44d07e5b48aab03ab759184f06c
Micha <michael.roeder@uni-paderborn.de>
2017-12-22 15:18:32 +0100

    Fixed tests that where not closing their resources correctly.

--- platform-controller/src/test/java/org/hobbit/controller/PlatformControllerTest.java
+++ platform-controller/src/test/java/org/hobbit/controller/PlatformControllerTest.java
@@ -26,6 +26,8 @@ import java.util.HashMap;
 import com.spotify.docker.client.DockerClient;
 import com.spotify.docker.client.messages.Container;
 import com.spotify.docker.client.messages.ContainerConfig;
+
+import org.apache.commons.io.IOUtils;
 import org.hobbit.controller.docker.ContainerManagerImpl;
 import org.hobbit.core.Commands;
 import org.hobbit.core.Constants;
@@ -62,36 +64,38 @@ public class PlatformControllerTest extends DockerBasedTest {
         }
     }
 
+    public void close() throws Exception {
+        IOUtils.closeQuietly(controller);
+        super.close();
+    }
+
     @Test
     public void receiveCommand() throws Exception {
         byte command = Commands.DOCKER_CONTAINER_START;
 
         // create and execute parent container
-        Map<String,String> labels = new HashMap<String, String>();
+        Map<String, String> labels = new HashMap<String, String>();
         labels.put(ContainerManagerImpl.LABEL_TYPE, Constants.CONTAINER_TYPE_SYSTEM);
-        final String parentId = dockerClient.createContainer(ContainerConfig.builder()
-                .image("busybox")
-                .cmd("sh", "-c", "while :; do sleep 1; done")
-                .labels(labels)
-                .build()).id();
+        final String parentId = dockerClient.createContainer(ContainerConfig.builder().image("busybox")
+                .cmd("sh", "-c", "while :; do sleep 1; done").labels(labels).build()).id();
         dockerClient.startContainer(parentId);
         final String parentName = dockerClient.inspectContainer(parentId).name();
 
         // create and execute test container
         final String image = "busybox:latest";
         final String type = Constants.CONTAINER_TYPE_SYSTEM;
-        byte[] data = ("{\"image\": \"" + image + "\", \"type\": \"" + type + "\", \"parent\": \"" + parentName + "\"}").getBytes(StandardCharsets.UTF_8);
+        byte[] data = ("{\"image\": \"" + image + "\", \"type\": \"" + type + "\", \"parent\": \"" + parentName + "\"}")
+                .getBytes(StandardCharsets.UTF_8);
         controller.receiveCommand(command, data, "1", "");
 
         // get running containers
         String containerId = null;
         List<Container> containers = dockerClient.listContainers(DockerClient.ListContainersParam.allContainers());
-        for(Container c : containers) {
+        for (Container c : containers) {
             String gotImage = c.image();
             String gotType = c.labels().get(ContainerManagerImpl.LABEL_TYPE);
             String gotParent = c.labels().get(ContainerManagerImpl.LABEL_PARENT);
-            if (gotImage != null && gotImage.equals(image)
-                    && gotType != null && gotType.equals(type)
+            if (gotImage != null && gotImage.equals(image) && gotType != null && gotType.equals(type)
                     && gotParent != null && gotParent.equals(parentId)) {
                 containerId = c.id();
                 break;
@@ -101,16 +105,20 @@ public class PlatformControllerTest extends DockerBasedTest {
         // cleanup
         try {
             dockerClient.stopContainer(containerId, 5);
-        } catch (Exception e) {}
+        } catch (Exception e) {
+        }
         try {
             dockerClient.removeContainer(containerId);
-        } catch (Exception e) {}
+        } catch (Exception e) {
+        }
         try {
             dockerClient.stopContainer(parentId, 5);
-        } catch (Exception e) {}
+        } catch (Exception e) {
+        }
         try {
             dockerClient.removeContainer(parentId);
-        } catch (Exception e) {}
+        } catch (Exception e) {
+        }
 
         // check that container exists
         assertNotNull(containerId);
