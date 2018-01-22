1d6f5ea3744bded4a3e0ea46ef8455348644e090
Marc Zöller <m.zoller@usu.de>
2018-01-22 11:12:31 +0100

    Add missing loading functionality
    Fix #176

--- hobbit-gui/gui-client/src/app/experiments/details/details.component.ts
+++ hobbit-gui/gui-client/src/app/experiments/details/details.component.ts
@@ -21,6 +21,7 @@ export class DetailsComponent implements OnInit, OnChanges {
   @Input()
   challengeTaskId: string;
 
+  loaded: Boolean;
   experiments: Experiment[];
   rows: TableRow[];
   rowGroups: string[];
@@ -32,6 +33,7 @@ export class DetailsComponent implements OnInit, OnChanges {
 
   ngOnChanges() {
     this.rows = null;
+    this.loaded = false;
 
     this.bs.queryExperiments(this.idsCommaSeparated, this.challengeTaskId).subscribe(data => {
       this.experiments = data;
@@ -42,6 +44,7 @@ export class DetailsComponent implements OnInit, OnChanges {
       if (this.experiments.length !== 0)
         this.buildTableRows();
     });
+    this.loaded = true;
   }
 
   private buildTableRows() {
