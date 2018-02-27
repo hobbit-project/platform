8430627fcc91e93553bf80ad79df88a542317ad6
Micha <michael.roeder@uni-paderborn.de>
2018-02-27 17:29:35 +0100

    Added env variable keys to set the host and port of Prometheus.

--- platform-controller/src/main/java/org/hobbit/controller/docker/ResourceInformationCollector.java
+++ platform-controller/src/main/java/org/hobbit/controller/docker/ResourceInformationCollector.java
@@ -1,11 +1,10 @@
 package org.hobbit.controller.docker;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,6 +15,8 @@ import org.hobbit.core.data.usage.CpuStats;
 import org.hobbit.core.data.usage.DiskStats;
 import org.hobbit.core.data.usage.MemoryStats;
 import org.hobbit.core.data.usage.ResourceUsageInformation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -23,8 +24,6 @@ import com.google.gson.JsonParser;
 import com.spotify.docker.client.DockerClient;
 import com.spotify.docker.client.messages.swarm.Task;
 import com.spotify.docker.client.messages.swarm.TaskStatus;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * A class that can collect resource usage information for the containers known
@@ -37,15 +36,43 @@ public class ResourceInformationCollector {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ResourceInformationCollector.class);
 
+    public static final String PROMETHEUS_HOST_KEY = "PROMETHEUS_HOST";
+    public static final String PROMETHEUS_PORT_KEY = "PROMETHEUS_PORT";
+
+    public static final String PROMETHEUS_HOST_DEFAULT = "http://prometheus";
+    public static final String PROMETHEUS_PORT_DEFAULT = "9090";
+
     private ContainerManager manager;
+    private String prometheusHost;
+    private String prometheusPort;
 
     public ResourceInformationCollector(ContainerManager manager) {
+        this(manager, null, null);
+    }
+
+    public ResourceInformationCollector(ContainerManager manager, String prometheusHost, String prometheusPort) {
         this.manager = manager;
+        this.prometheusHost = prometheusHost;
+        if ((this.prometheusHost == null) && System.getenv().containsKey(PROMETHEUS_HOST_KEY)) {
+            this.prometheusHost = System.getenv().get(PROMETHEUS_HOST_KEY);
+        }
+        if (this.prometheusHost == null) {
+            LOGGER.info("Didn't got the prometheus host. Using default {}.", PROMETHEUS_HOST_DEFAULT);
+            this.prometheusHost = PROMETHEUS_HOST_DEFAULT;
+        }
+        this.prometheusPort = prometheusHost;
+        if ((this.prometheusPort == null) && System.getenv().containsKey(PROMETHEUS_PORT_KEY)) {
+            this.prometheusPort = System.getenv().get(PROMETHEUS_PORT_KEY);
+        }
+        if (this.prometheusPort == null) {
+            LOGGER.info("Didn't got the prometheus port. Using default {}.", PROMETHEUS_PORT_DEFAULT);
+            this.prometheusPort = PROMETHEUS_PORT_DEFAULT;
+        }
     }
 
     public ResourceUsageInformation getSystemUsageInformation() {
-        return getUsageInformation(
-                Task.Criteria.builder().label(ContainerManager.LABEL_TYPE + "=" + Constants.CONTAINER_TYPE_SYSTEM).build());
+        return getUsageInformation(Task.Criteria.builder()
+                .label(ContainerManager.LABEL_TYPE + "=" + Constants.CONTAINER_TYPE_SYSTEM).build());
     }
 
     public ResourceUsageInformation getUsageInformation(Task.Criteria criteria) {
@@ -68,20 +95,21 @@ public class ResourceInformationCollector {
     protected ResourceUsageInformation requestCpuAndMemoryStats(String containerId) {
         ResourceUsageInformation resourceInfo = new ResourceUsageInformation();
         try {
-            resourceInfo.setCpuStats(new CpuStats(Math.round(Double.parseDouble(
-                    requestPrometheusValue(containerId, "container_cpu_usage_seconds_total"))*1000)));
+            resourceInfo.setCpuStats(new CpuStats(Math
+                    .round(Double.parseDouble(requestPrometheusValue(containerId, "container_cpu_usage_seconds_total"))
+                            * 1000)));
         } catch (Exception e) {
             LOGGER.error("Could not get cpu usage stats for container {}", containerId, e);
         }
         try {
-            resourceInfo.setMemoryStats(new MemoryStats(Long.parseLong(
-                    requestPrometheusValue(containerId, "container_memory_usage_bytes"))));
+            resourceInfo.setMemoryStats(new MemoryStats(
+                    Long.parseLong(requestPrometheusValue(containerId, "container_memory_usage_bytes"))));
         } catch (Exception e) {
             LOGGER.error("Could not get memory usage stats for container {}", containerId, e);
         }
         try {
-            resourceInfo.setDiskStats(new DiskStats(Long.parseLong(
-                    requestPrometheusValue(containerId, "container_fs_usage_bytes"))));
+            resourceInfo.setDiskStats(
+                    new DiskStats(Long.parseLong(requestPrometheusValue(containerId, "container_fs_usage_bytes"))));
         } catch (Exception e) {
             LOGGER.error("Could not get disk usage stats for container {}", containerId, e);
         }
@@ -90,7 +118,7 @@ public class ResourceInformationCollector {
 
     private String requestPrometheusValue(String taskId, String metric) throws IOException, MalformedURLException {
         String filter = "{container_label_com_docker_swarm_task_id=\"" + taskId + "\"}";
-        URL url = new URL("http://prometheus:9090/api/v1/query?query=" + metric + filter);
+        URL url = new URL(prometheusHost + ":" + prometheusPort + "/api/v1/query?query=" + metric + filter);
         BufferedReader reader = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream()));
         String line;
         String content = "";
