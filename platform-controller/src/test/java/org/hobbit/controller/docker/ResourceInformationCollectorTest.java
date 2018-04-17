32841b73fb9d834992cc39f39d77e063bc5e3c5e
Micha <michael.roeder@uni-paderborn.de>
2018-04-17 15:21:58 +0200

    Decreased the breaks between the single updates of prometheus. Extended the test to cover a typical use case.

--- platform-controller/src/test/java/org/hobbit/controller/docker/ResourceInformationCollectorTest.java
+++ platform-controller/src/test/java/org/hobbit/controller/docker/ResourceInformationCollectorTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotNull;
 import org.hobbit.core.Constants;
 import org.hobbit.core.data.usage.ResourceUsageInformation;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.EnvironmentVariables;
@@ -18,12 +19,16 @@ public class ResourceInformationCollectorTest extends ContainerManagerBasedTest 
     @Rule
     public final EnvironmentVariables environmentVariables = new EnvironmentVariables();
 
-    @Test
-    public void test() throws Exception {
+    @Before
+    public void setEnv() {
         environmentVariables.set(ResourceInformationCollector.PROMETHEUS_HOST_KEY, "localhost");
         environmentVariables.set(ResourceInformationCollector.PROMETHEUS_PORT_KEY,
                 ResourceInformationCollector.PROMETHEUS_PORT_DEFAULT);
+    }
 
+    @Test
+    public void test() throws Exception {
+        LOGGER.info("Creating container...");
         String containerId = manager.startContainer(busyboxImageName, Constants.CONTAINER_TYPE_SYSTEM, null,
                 sleepCommand);
         assertNotNull(containerId);
@@ -32,6 +37,7 @@ public class ResourceInformationCollectorTest extends ContainerManagerBasedTest 
         Thread.sleep(10000);
 
         ResourceInformationCollector collector = new ResourceInformationCollector(manager);
+        LOGGER.info("Requesting usage information...");
         ResourceUsageInformation usage = collector.getSystemUsageInformation();
 
         Assert.assertNotNull(usage);
@@ -45,12 +51,14 @@ public class ResourceInformationCollectorTest extends ContainerManagerBasedTest 
         Assert.assertTrue(usage.getDiskStats().getFsSizeSum() > 0);
 
         // Generate a second container
+        LOGGER.info("Creating second container...");
         containerId = manager.startContainer(busyboxImageName, Constants.CONTAINER_TYPE_SYSTEM, null, sleepCommand);
         assertNotNull(containerId);
         tasks.add(containerId);
 
         Thread.sleep(10000);
 
+        LOGGER.info("Requesting usage information...");
         ResourceUsageInformation usage2 = collector.getSystemUsageInformation();
         Assert.assertNotNull(usage2);
         LOGGER.info("Got usage information {}", usage2);
@@ -62,5 +70,53 @@ public class ResourceInformationCollectorTest extends ContainerManagerBasedTest 
         Assert.assertTrue(usage.getMemoryStats().getUsageSum() <= usage2.getMemoryStats().getUsageSum());
         Assert.assertNotNull(usage2.getDiskStats());
         Assert.assertTrue(usage.getDiskStats().getFsSizeSum() <= usage2.getDiskStats().getFsSizeSum());
-    }   // "dd if=/dev/zero of=file.txt count=1024 bs=1048576"
+    } //
+
+    @Test
+    public void testIncreasingFsSize() throws Exception {
+        // final String[] command = { "sleep", "30s", "&&", "dd", "if=/dev/zero",
+        // "of=file.txt", "count=1024", "bs=1048576", "&&", "sleep", "60s" };
+        ResourceInformationCollector collector = new ResourceInformationCollector(manager);
+        final String[] command = { "sh", "-c",
+                "sleep 20s ; dd if=/dev/zero of=file.txt count=1024 bs=1048576 ; sleep 60s" };
+        LOGGER.info("Creating container...");
+        String containerId = manager.startContainer(busyboxImageName, Constants.CONTAINER_TYPE_SYSTEM, null, command);
+        assertNotNull(containerId);
+        tasks.add(containerId);
+        // Waiting for the service to create and start its container
+        Thread.sleep(10000);
+
+        LOGGER.info("Requesting usage information...");
+        ResourceUsageInformation usage = collector.getSystemUsageInformation();
+
+        Assert.assertNotNull(usage);
+        LOGGER.info("Got usage information {}", usage);
+
+        Assert.assertNotNull(usage.getCpuStats());
+        Assert.assertTrue(usage.getCpuStats().getTotalUsage() > 0);
+        Assert.assertNotNull(usage.getMemoryStats());
+        Assert.assertTrue(usage.getMemoryStats().getUsageSum() > 0);
+        Assert.assertNotNull(usage.getDiskStats());
+        Assert.assertTrue(usage.getDiskStats().getFsSizeSum() > 0);
+
+        LOGGER.info("Waiting for the container to generate its file...");
+        Thread.sleep(30000);
+
+        LOGGER.info("Requesting usage information...");
+        ResourceUsageInformation usage2 = collector.getSystemUsageInformation();
+        Assert.assertNotNull(usage2);
+        LOGGER.info("Got usage information {}", usage2);
+
+        Assert.assertNotNull(usage2.getCpuStats());
+        Assert.assertTrue(usage2.getCpuStats().getTotalUsage() > 0);
+        // Assert.assertTrue("We expected that the CPU time used to generate the file
+        // would increase the overall CPU time",
+        // usage.getCpuStats().getTotalUsage() < usage2.getCpuStats().getTotalUsage());
+        Assert.assertNotNull(usage2.getMemoryStats());
+        // Assert.assertTrue(usage.getMemoryStats().getUsageSum() <
+        // usage2.getMemoryStats().getUsageSum());
+        Assert.assertNotNull(usage2.getDiskStats());
+        Assert.assertTrue("We expected that the Fssize would be increased when generating a huge file",
+                usage.getDiskStats().getFsSizeSum() < usage2.getDiskStats().getFsSizeSum());
+    }
 }
