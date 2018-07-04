00a9c22f6e57efa928e8ba7e505711ad9e878043
Ivan Ermilov <earthquakesan@gmail.com>
2018-07-04 16:08:49 +0200

    check for number of running virtuoso instances before running init script

--- run-storage-init.sh
+++ run-storage-init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Runs the initialization script for the Storage of the HOBBIT Platform.
 VOS_CONTAINER_ID=$(docker ps -a | grep vos | cut -d' ' -f 1)
-echo $VOS_CONTAINER_ID
-docker exec -it $VOS_CONTAINER_ID bash ./storage-init.sh
+VOS_CONTAINER_NUMBER=$(docker ps -a | grep vos | cut -d' ' -f 1 | wc -l)
+if [ "$VOS_CONTAINER_NUMBER" = "1" ]; then
+  docker exec -it $VOS_CONTAINER_ID bash ./storage-init.sh
+else
+  echo "Can not determine vos container name..."
+  echo "Use docker ps to determine vos container name and execute the following command manually:"
+  echo "docker exec -it yourvoscontainername bash ./storage-init.sh"
+fi
