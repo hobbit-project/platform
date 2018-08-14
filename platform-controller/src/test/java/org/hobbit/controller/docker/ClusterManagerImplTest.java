5d32545b47d947b121dedd937ff23875877227cc
Micha <michael.roeder@uni-paderborn.de>
2018-08-14 12:49:06 +0200

    Increased the core library version. Fixed the ClusterManagerImplTest class.

--- platform-controller/src/test/java/org/hobbit/controller/docker/ClusterManagerImplTest.java
+++ platform-controller/src/test/java/org/hobbit/controller/docker/ClusterManagerImplTest.java
@@ -23,8 +23,8 @@ public class ClusterManagerImplTest {
 
     @Test
     public void getNumberOfNodes() throws Exception {
-        Integer numberOfNodes = clusterManager.getNumberOfNodes();
-        assertTrue(numberOfNodes == 1);
+        long numberOfNodes = clusterManager.getNumberOfNodes();
+        assertEquals(1, numberOfNodes);
     }
 
     @Test
@@ -37,7 +37,7 @@ public class ClusterManagerImplTest {
     public void setTaskHistoryLimit() throws Exception {
         clusterManager.setTaskHistoryLimit(0);
         Integer taskHistoryLimit = clusterManager.getTaskHistoryLimit();
-        assertTrue(taskHistoryLimit == 0);
+        assertEquals(0, (long) taskHistoryLimit);
         //set back to default
         clusterManager.setTaskHistoryLimit(5);
     }
