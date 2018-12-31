aec68f4984912df408dd21d3051516929b84b5c4
Denis Kuchelev <denkv@users.noreply.github.com>
2018-12-31 14:16:12 +0300

    Display analysis results with a spider chart

---
+++ hobbit-gui/gui-client/src/app/analysis/analysis.component.ts
@@ -0,0 +1,75 @@
+import {MessageService} from 'primeng/components/common/messageservice';
+import {ConfigParamDefinition, ConfigParamRealisation, AnalysisResultset, Benchmark} from '../model';
+import {Router} from '@angular/router';
+import {BackendService} from '../backend.service';
+import {Component, ElementRef, Input, OnChanges, OnInit, ViewChild} from '@angular/core';
+import * as Chart from 'chart.js';
+import {scaleOrdinal} from 'd3-scale';
+import {schemeCategory10} from 'd3-scale-chromatic';
+
+@Component({
+  selector: 'app-analysis',
+  templateUrl: './analysis.component.html',
+})
+export class AnalysisComponent implements OnChanges, OnInit {
+
+  @Input()
+  benchmark: Benchmark;
+
+  @ViewChild('canvas')
+  canvas: ElementRef;
+
+  resultsets: AnalysisResultset[];
+  kpis: ConfigParamRealisation[];
+  selectedKPI: ConfigParamRealisation;
+  configModel: any = {};
+
+  constructor(private bs: BackendService, private router: Router,
+    private messageService: MessageService) {
+  }
+
+  ngOnInit() {
+  }
+
+  ngOnChanges() {
+    this.bs.queryAnalysisResults(this.benchmark.id).subscribe(data => {
+      this.resultsets = data;
+      this.kpis = this.resultsets[0].benchmark.kpis;
+    });
+  }
+
+  onChangeKPI(event) {
+    this.selectedKPI = this.kpis.find(k => k.id === event);
+    this.drawChart();
+  }
+
+  drawChart() {
+    const chartCanvas = <HTMLCanvasElement>this.canvas.nativeElement;
+    const ctx = chartCanvas.getContext('2d');
+    const colorScale = scaleOrdinal(schemeCategory10);
+
+    const parameters = this.resultsets[0].benchmark.configurationParams;
+    const labels = parameters.map(kpi => kpi.name);
+    const datasets = this.resultsets.map((resultset, index) => ({
+      backgroundColor: 'transparent',
+      borderColor: colorScale(index),
+      data: parameters.map(parameter => resultset.results.find(result => result.kpiUri === this.selectedKPI.id && result.parameterUri === parameter.id).value),
+      label: resultset.system.name,
+    }));
+
+    const chart = new Chart.Chart(ctx, {
+      type: 'radar',
+      data: {
+        datasets,
+        labels,
+      },
+      options: {
+        legend: {position: 'right'},
+        responsive: true,
+        scale: {
+          ticks: {min: -1, max: 1},
+        },
+      }
+    });
+  }
+}
