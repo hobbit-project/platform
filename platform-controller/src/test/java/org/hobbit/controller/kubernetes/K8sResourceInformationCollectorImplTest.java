bbbb6468035d0491881f021f0c4816298292d0f9
dejiabiodun <oyedeleabiodun.s@gmail.com>
2021-01-21 22:23:19 +0100

    start container container manager Test cases.

---
+++ platform-controller/src/test/java/org/hobbit/controller/kubernetes/K8sResourceInformationCollectorImplTest.java
@@ -0,0 +1,24 @@
+package org.hobbit.controller.kubernetes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class K8sResourceInformationCollectorImplTest {
+
+    @Test
+    void getSystemUsageInformation() {
+    }
+
+    @Test
+    void getUsageInformation() {
+    }
+
+    @Test
+    void requestCpuAndMemoryStats() {
+    }
+
+    @Test
+    void getHardwareInformation() {
+    }
+}
