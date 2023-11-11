8c9719834fd4782adf38dec1efb6c7bda14a9356
Michael Röder <michael.roeder@uni-paderborn.de>
2023-11-11 12:41:58 +0100

    Adapted file persmissions.

--- config/db/storage-init.sh
+++ config/db/storage-init.sh
@@ -1,9 +1,6 @@
 # Initialization script for the Storage of the HOBBIT Platform.
 # Controls access rights for Platform-specific RDF graphs.
 
-echo "Waiting for port to open..."
-while ! nc -q 1 localhost 1111 </dev/null; do sleep 5; done
-
 # Limit access rights of anonymous users to RDF graphs in the Storage
 /opt/virtuoso-opensource/bin/isql 1111 dba dba exec="DB.DBA.RDF_DEFAULT_USER_PERMS_SET ('nobody', 0);"
 
